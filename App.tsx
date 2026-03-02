af51acdb728f086a6889f99f9d4c2d8d70bc8452
Vermin84 <verminator84@gmail.com>
2026-03-02 13:29:04 +0200

    setting project? add navigation

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Start project</Text>
+      <Text>Start project </Text>
     </View>
   );
 }
