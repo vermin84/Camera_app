1f65c64fd14d36d82b63719c9147ab8ababd7a8e
Vermin84 <verminator84@gmail.com>
2026-03-05 12:42:10 +0200

    add navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CameraScreen from "./screens/CameraScreen";
+import GalleryScreen from "./screens/GalleryScreen";
+const Stack = createNativeStackNavigator();
+
+
+
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Start project </Text>
-    </View>
-  );
+  return <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Camera" component={CameraScreen} />
+        <Stack.Screen name="Gallery" component={GalleryScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
 }
---
+++ screens/GalleryScreen.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+
+export default function GalleryScreen(){
+    return <View>
+        <Text>Gallery screen</Text>
+    </View>
+}
